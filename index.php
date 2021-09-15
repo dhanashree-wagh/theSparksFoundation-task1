4cbbf3e639bbfb0d7c350ff0f1072aeb08f4e828
dhanashree-wagh <84534754+dhanashree-wagh@users.noreply.github.com>
2021-09-15 19:14:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@800&display=swap" rel="stylesheet">
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+<style>
+  body{
+    background:url("https://images.unsplash.com/photo-1616514197671-15d99ce7a6f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGJhbmt8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60")  ;
+    background-size: cover;
+    
+  }
+  .btn{
+    margin: 30vh;
+    margin-left:7vh;
+    
+    
+  }
+  
+ 
+</style>
+    <title>spark foundation basic banking system</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  <div class="btn">
+
+  
+                    <a href="transactionhistory.php"><button >Transfer History</button></a>
+                  
+                    <a href="transfermoney.php"><button>Select Customers to transfer</button></a>
+                    </div>
+     
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.php">DHANASHREE BANK OF PUNE | PIGGY BANK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">HISTORY</a>
+              </li>
+          </div>
+       </nav>
